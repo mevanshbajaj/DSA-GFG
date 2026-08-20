6654e6445aaa6d1e342197c9999ccbe1a5dd8e1a
Vansh Bajaj <mevanshbajaj@gmail.com>
2026-08-20 22:27:09 +0530

    new q

--- CountInversions.java
+++ CountInversions.java
@@ -1,49 +1,43 @@
-class Solution { 
-    static int mergesort(int[] arr , int s , int e){
-        if(s>=e) return 0;
-        int mid = (s+e)/2;
-        int leftinversion = mergesort(arr,s,mid);
-        int rightinversion = mergesort(arr,mid+1,e);
-        int mergeinversion = merge(arr,s,e,mid);
-        return leftinversion+rightinversion+mergeinversion;
+class Solution {
+    static int mergesort(int[] arr, int s, int e) {
+        if (s >= e) return 0;
+        int mid = s + (e - s) / 2;
+        int leftinversion = mergesort(arr, s, mid);
+        int rightinversion = mergesort(arr, mid + 1, e);
+        int mergeinversion = merge(arr, s, e, mid);
+        return leftinversion + rightinversion + mergeinversion;
     }
-    static int merge(int[] arr, int s , int e , int mid){
-        // leftarray and right array create
-        int leftlen = mid-s+1;
-        int rightlen = e-mid;
-        int leftarr = new int[leftlen];
-        int rightarr = new int[rightlen];
-        int k = s; // main array index
-        for(int i=0;i<leftlen;i++){
-            leftarr[i] = arr[k];
-            k++;
-        }
-        // k = mid+1;
-        for(int j=0;j<rightlen;j++){
-            rightarr[j] = arr[k];
-            k++;
-        }
-        int i = 0 , j = 0 ;
+    static int merge(int[] arr, int s, int e, int mid) {
+        int leftlen = mid - s + 1;
+        int rightlen = e - mid;
+        int[] leftarr = new int[leftlen];
+        int[] rightarr = new int[rightlen];
+        int k = s;
+        for (int i = 0; i < leftlen; i++) leftarr[i] = arr[k++];
+        for (int j = 0; j < rightlen; j++) rightarr[j] = arr[k++];
+        
+        int i = 0, j = 0;
         k = s;
-        while(i<leftlen && j < rightlen){
-            if(leftarr[i] <= rightarr[j]){
-                arr[k] = leftarr[i];
-                k++;i++;
-            }
-            else{
-                // leftarr[i] > rightarr[j]
-                // merge logic 
-                arr[k] = rightarr[j];
-                j++;k++;
-                //inversion logic
-                int inversion = leftlen-i;
+        int count = 0;
+        while (i < leftlen && j < rightlen) {
+            if (leftarr[i] <= rightarr[j]) {
+                arr[k++] = leftarr[i++];
+            } 
+            else {
+                arr[k++] = rightarr[j++];
+                // All remaining elements in leftarr
+                // form an inversion with rightarr[j-1]
+                count += leftlen - i;
             }
         }
+        while (i < leftlen) arr[k++] = leftarr[i++];
+        while (j < rightlen) arr[k++] = rightarr[j++];
+        return count;
     }
     public int inversionCount(int arr[]) {
-        // code here
         int s = 0;
-        int e = arr.length-1;
-        return mergesort(arr,s,e);
+        int e = arr.length - 1;
+
+        return mergesort(arr, s, e);
     }
 }
