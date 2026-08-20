7c0b3e2340d4a72732ab0c195df5b246bf156e0b
Vansh Bajaj <mevanshbajaj@gmail.com>
2026-08-20 21:25:16 +0530

    new q

--- CountInversions.java
+++ CountInversions.java
@@ -1,3 +1,13 @@
-public class CountInversions {
-    
+class Solution {
+    public int inversionCount(int arr[]) {
+        // code here
+        int n = arr.length;
+        int count = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                    if(arr[i] > arr[j]) count++;
+            }
+        }
+        return count;
+    }
 }
