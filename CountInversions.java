8a2d60e0ea5913b7345381d341573ff5092603b6
Vansh Bajaj <mevanshbajaj@gmail.com>
2026-08-20 22:21:13 +0530

    new q

--- CountInversions.java
+++ CountInversions.java
@@ -1,13 +1,49 @@
-class Solution {
-    public int inversionCount(int arr[]) {
-        // code here
-        int n = arr.length;
-        int count = 0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                    if(arr[i] > arr[j]) count++;
+class Solution { 
+    static int mergesort(int[] arr , int s , int e){
+        if(s>=e) return 0;
+        int mid = (s+e)/2;
+        int leftinversion = mergesort(arr,s,mid);
+        int rightinversion = mergesort(arr,mid+1,e);
+        int mergeinversion = merge(arr,s,e,mid);
+        return leftinversion+rightinversion+mergeinversion;
+    }
+    static int merge(int[] arr, int s , int e , int mid){
+        // leftarray and right array create
+        int leftlen = mid-s+1;
+        int rightlen = e-mid;
+        int leftarr = new int[leftlen];
+        int rightarr = new int[rightlen];
+        int k = s; // main array index
+        for(int i=0;i<leftlen;i++){
+            leftarr[i] = arr[k];
+            k++;
+        }
+        // k = mid+1;
+        for(int j=0;j<rightlen;j++){
+            rightarr[j] = arr[k];
+            k++;
+        }
+        int i = 0 , j = 0 ;
+        k = s;
+        while(i<leftlen && j < rightlen){
+            if(leftarr[i] <= rightarr[j]){
+                arr[k] = leftarr[i];
+                k++;i++;
+            }
+            else{
+                // leftarr[i] > rightarr[j]
+                // merge logic 
+                arr[k] = rightarr[j];
+                j++;k++;
+                //inversion logic
+                int inversion = leftlen-i;
             }
         }
-        return count;
+    }
+    public int inversionCount(int arr[]) {
+        // code here
+        int s = 0;
+        int e = arr.length-1;
+        return mergesort(arr,s,e);
     }
 }
