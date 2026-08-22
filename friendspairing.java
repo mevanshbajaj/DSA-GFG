d141a6afca48a39ded3a6b846d3896fb7859b967
Vansh Bajaj <mevanshbajaj@gmail.com>
2026-08-22 16:55:44 +0530

    dp

--- friendspairing.java
+++ friendspairing.java
@@ -1,13 +1,11 @@
 class Solution {
-    static int[] dp;
     public int countFriendsPairings(int n) {
-        dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return solve(n);
-    }
-    private int solve(int n) {
-        if (n <= 2) return n;
-        if (dp[n] != -1) return dp[n];
-        return dp[n] = solve(n - 1) + (n - 1) * solve(n - 2);
+        // code here
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        if (n > 1) dp[2] = 2;
+        for(int i=3;i<=n;i++)
+        dp[i] = dp[i-1] + (i-1)*dp[i-2];
+        return dp[n];
     }
 }
