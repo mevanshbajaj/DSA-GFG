0ee92ba99197813f7918f84f4bf1ec7345361474
Vansh Bajaj <mevanshbajaj@gmail.com>
2026-08-24 20:46:00 +0530

    dp

--- countDearrangements.java
+++ countDearrangements.java
@@ -3,6 +3,12 @@ class Solution {
         // code here
         if (n == 0) return 1;
         if (n == 1) return 0;
-        return (n - 1) * (derangeCount(n - 1) + derangeCount(n - 2));
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (i - 1) * (dp[i - 1] + dp[i - 2]);
+        }
+        return dp[n];
     }
 };
