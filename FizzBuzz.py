ee6911e54899712c9c81ef0f466bf6a64bfcf349
Vansh Bajaj <mevanshbajaj@gmail.com>
2026-09-02 20:25:32 +0530

    python

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,14 +1,12 @@
-
-class Solution {
-    public static ArrayList<String> fizzBuzz(int n) {
-        // code here
-        ArrayList<String> result = new ArrayList<>();
-        for(int i=1;i<=n;i++){
-            if(i % 5 == 0 && i % 3 == 0) result.add("FizzBuzz");
-            else if(i % 3 == 0) result.add("Fizz");
-            else if(i % 5 == 0) result.add("Buzz");
-            else result.add(String.valueOf(i));
-        }
-        return result;
-    }
-}
+class Solution:
+    def fizzBuzz(self, n : int):
+        # code here
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                print("FizzBuzz", end = " ")
+            elif i % 3 == 0:
+                print("Fizz", end = " ")
+            elif i % 5 == 0:
+                print("Buzz", end = " ")
+            else:
+                print(i, end = " ")
